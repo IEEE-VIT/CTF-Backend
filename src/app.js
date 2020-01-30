851f2470d145b9964e4476be78ef1529b26fde29
iamyuvi2000 <yuvraj.singh2018@vitstudent.ac.in>
2020-01-30 23:09:26 +0530

    integrated firebase

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ const app = express();
 const router = express.Router();
 var cors = require('cors');
 app.use(cors());
+const chalk = require('chalk');
 const dotenv = require("dotenv");
 dotenv.config();
-
+// const {admin,database} = require("./utils/firebase")
+// const userRef = database.collection('Users').doc('hello')
+//         userRef.set({
+//             uid: 'user.uid',
+//             name: 'user.name',
+//             email: 'user.email',
+//         })
+//         .then(( resp )=>{
+//             console.log(chalk.green("New user details saved in db"))
+//         })
 const port = process.env.PORT
 app.listen(port,()=>{
     console.log('Server is up on port ',port)
