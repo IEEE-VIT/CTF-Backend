2b2136edbfcf3e0488da1a3b31d65a543804d7c8
iamyuvi2000 <yuvraj.singh2018@vitstudent.ac.in>
2020-01-31 01:45:25 +0530

    Integrated userCreate middleware and userCreate Route

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require("body-parser")
 var cors = require('cors');
 app.use(cors());
 const chalk = require('chalk');
 const dotenv = require("dotenv");
 dotenv.config();
-// const {admin,database} = require("./utils/firebase")
-// const userRef = database.collection('Users').doc('hello')
-//         userRef.set({
-//             uid: 'user.uid',
-//             name: 'user.name',
-//             email: 'user.email',
-//         })
-//         .then(( resp )=>{
-//             console.log(chalk.green("New user details saved in db"))
-//         })
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Route imports
+const userRoute = require('./routes/user');
+
+//Use Routes
+app.use('/user',userRoute);
+
 const port = process.env.PORT
 app.listen(port,()=>{
     console.log('Server is up on port ',port)
