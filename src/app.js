1536f20551b2fe5d064da7f33aa961237197b67a
Yuvraj Singh <yuvraj.singh2018@vitstudent.ac.in>
2020-02-01 20:10:16 +0530

    merge conflict

--- src/app.js
+++ src/app.js
@@ -7,16 +7,6 @@ app.use(cors());
 const chalk = require('chalk');
 const dotenv = require("dotenv");
 dotenv.config();
-<<<<<<< HEAD
-// const {admin,database} = require("./utils/firebase")
-// const userRef = database.collection('Users').doc('hello')
-//         userRef.update({
-//             uid: 'user',
-//         })
-//         .then(( resp )=>{
-//             console.log(chalk.green("New user details saved in db"))
-//         })
-=======
 
 
 app.use(bodyParser.json());
@@ -28,7 +18,6 @@ const userRoute = require('./routes/user');
 //Use Routes
 app.use('/user',userRoute);
 
->>>>>>> 2b2136edbfcf3e0488da1a3b31d65a543804d7c8
 const port = process.env.PORT
 app.listen(port,()=>{
     console.log('Server is up on port ',port)
