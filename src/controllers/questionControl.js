fe7fe1aa425b4302dda2983c3edfb164eec30ea4
Divyansh <divyanshkhandelwal147@gmail.com>
2020-02-03 01:20:31 +0530

    configuered boiler-plate code for controllers

--- src/controllers/questionControl.js
+++ src/controllers/questionControl.js
@@ -1,3 +1,53 @@
-const {admin,database} = require('../utils/firebase')
+const { admin, database } = require('../utils/firebase')
 const chalk = require('chalk')
 
+
+//controller for creating a question
+const createQuestion = (ques) => {
+    return new Promise((resolve, reject) => {
+
+    })
+}
+
+
+//controller for reading a question
+const readQuestion = (ques) => {
+    return new Promise((resolve, reject) => {
+
+    })
+}
+
+
+//controller for reading all the question
+const readAllQuestion = (ques) => {
+    return new Promise((resolve, reject) => {
+
+    })
+}
+
+
+//controller for updating a question
+const updateQuestion = (ques) => {
+    return new Promise((resolve, reject) => {
+
+    })
+}
+
+
+//controller for deleting a question
+const deleteQuestion = (ques) => {
+    return new Promise((resolve, reject) => {
+
+    })
+}
+
+
+
+
+module.exports = {
+    createQuestion,
+    readQuestion,
+    readAllQuestion,
+    updateQuestion,
+    deleteQuestion
+}
