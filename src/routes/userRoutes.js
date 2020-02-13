e4e5abd001fbc0f2937f3cda2e9206579a197515
Divyansh <divyanshkhandelwal147@gmail.com>
2020-02-13 11:12:02 +0530

    code for /profile

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -16,4 +16,9 @@ router.post('/hint',(req,res)=>{
     .catch(err => res.status(400).send(err))
 })
 
+// router.post('/profile', (req, res)=> {
+//     .then(resp=>res.status(200).send(resp))
+//     .catch(err => res.status(400).send(err))
+// })
+
 module.exports=router;
