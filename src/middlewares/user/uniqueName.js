06d3820b3a7ac34a2d313eec7ce3ef54015a2f4e
Divyansh <divyanshkhandelwal147@gmail.com>
2020-04-26 14:24:14 +0530

    Unique usename check

--- src/middlewares/user/uniqueName.js
+++ src/middlewares/user/uniqueName.js
@@ -1,9 +1,42 @@
 const { admin, database } = require('../../utils/firebase')
 
-const uniqueName = (req, res, next) => {
+const uniqueName = async (req, res, next) => {
+    console.log('Unique Name Check')
     const userRef = database.collection('Users')
-    var names = []
-    console.log(userRef)
+    await userRef.get()
+        .then(snap => {
+            allUsers = []
+            snap.forEach(doc => {
+                const id = doc.id;
+                const username = doc.data().userName
+                allUsers.push(
+                    username
+                )
+            })
+            return (allUsers)
+        })
+        .then((users) => {
+            const index = users.indexOf(req.body.userName)
+            if (index < 0) {
+                next()
+            } else {
+                res.status(400).send({
+                    statusCode: 400,
+                    payload: {
+                        status: "User Name already Taken",
+                    }
+                })
+            }
+        })
+        .catch((e) => {
+            console.log("Error in Reading all the User details")
+            res.status(400).send({
+                status: 400,
+                payload: {
+                    msg: "Error in reading User data, Contact Support!"
+                }
+            })
+        })
 }
 
 module.exports = uniqueName
