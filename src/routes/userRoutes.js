0cadddfea3a3c6c66fb96b8ed98633ac0a040057
Divyansh <divyanshkhandelwal147@gmail.com>
2020-05-10 14:12:51 +0530

    hint route bug

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,9 +4,6 @@ const userCreate = require('../middlewares/user/userCreateMiddleware');
 const previouslySolved = require('../middlewares/universal/previouslySolved');
 const userAuth = require('../middlewares/user/userAuth');
 // const capcha = require('../middlewares/user/capcha');
-const uniqueName = require('../middlewares/user/uniqueName')
-const chalk = require('chalk')
-
 
 //route to create for a user
 router.post('/create', [userCreate], (req, res) => {
@@ -16,8 +13,34 @@ router.post('/create', [userCreate], (req, res) => {
 })
 
 
+//route to check the submitted answer
+router.post("/checkAnswer", [previouslySolved, userAuth], (req, res) => {
+    userControls.checkAnswer(req.body.uid, req.body.answer, req.body.questionId)
+        .then(resp => res.status(200).send(resp))
+        .catch(err => res.status(400).send(err))
+})
+
+
+//route to show the hint of a given question
+router.post('/hint', userAuth, (req, res) => {
+    var questionID = req.body.questionID;
+    var uid = req.body.uid;
+    userControls.fetchHint(questionID, uid)
+        .then(resp => res.status(200).send(resp))
+        .catch(err => res.status(400).send(err))
+})
+
+
+//route to get all questions on the globe
+router.get('/getAllQuestions', [userAuth], (req, res) => {
+    userControls.readAllQuestion()
+        .then(resp => res.status(200).send(resp))
+        .catch(err => res.status(400).send(err))
+})
+
+
 //route to show the user profile
-router.post('/profile', [userAuth], (req, res) => {
+router.get('/profile', [userAuth], (req, res) => {
     userControls.showProfile({
         "uid": req.body.uid
     })
@@ -27,40 +50,12 @@ router.post('/profile', [userAuth], (req, res) => {
 
 
 //route to update the user profile
-router.put('/updateProfile', [userAuth, uniqueName], (req, res) => {
+router.get('/updateProfile', [userAuth], (req, res) => {
     userControls.updateProfile({
         uid: req.body.uid,
-        userName: req.body.userName
+        name: req.body.name
     })
         .then(resp => res.status(200).send(resp))
-        .catch(err => res.status(400).send(err))
-})
-
-
-//route to check flags on submit
-router.post('/checkFlag', [userAuth, previouslySolved], (req, res) => {
-    userControls.checkAnswer(
-        req.body.uid,
-        req.body.flag,
-        req.body.id
-    )
-        .then(resp => res.status(200).send(resp))
-        .catch(err => res.status(400).send(err))
-})
-
-
-//route to get all questions on the globe
-router.post('/getAllQuestions', [userAuth], (req, res) => {
-    userControls.readAllQuestion()
-        .then(resp => res.status(200).send(resp))
-        .catch(err => res.status(400).send(err))
-})
-
-
-router.post('/leaderboard', (req, res) => {
-    console.log(chalk.yellow('Fetching Leaderboards...'))
-    userControls.getLeaderboard()
-        .then(resp => res.send(resp).status(200))
         .catch(err => res.status(400).send(err))
 })
 
