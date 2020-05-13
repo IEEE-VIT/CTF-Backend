2eee790b779f0579b5aed0df14566293a10d7509
Divyansh <divyanshkhandelwal147@gmail.com>
2020-05-14 02:44:01 +0530

    basic test for leaderboard and profile

--- test/user.test.js
+++ test/user.test.js
@@ -1,10 +1,24 @@
 const request = require('supertest')
 const app = require('../src/index')
-
+jest.setTimeout(30000);
 
 
 
 test('api/', async () => {
     await request(app).get('/')
+        .expect(200)
+})
+
+
+test('user/profile', async () => {
+    await request(app)
+        .post('/user/profile')
+        .set('Authorization', 'XAvpYLfU9eTCTfCkEvQIH8nXpy62')
+        .expect(200)
+})
+
+
+test('user/leaderboard', async () => {
+    await request(app).post('/user/leaderboard')
         .expect(200)
 })
