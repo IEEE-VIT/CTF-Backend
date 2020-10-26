fc54b32cc453058b3f0a8b3b84852945076cc333
ZiyanK <ziyankarmali786@gmail.com>
2020-10-27 02:22:22 +0530

    Added rate limiter config comment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 dotenv.config();
 
+//Added rate limiter config
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
